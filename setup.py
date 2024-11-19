380302c4c73c6e686180f2b64afef78bf526fd4f
rpg59 <vanxellshing24@gmail.com>
2024-11-20 00:04:30 +0300

    update

--- setup.py
+++ setup.py
@@ -15,19 +15,29 @@ def packages():
             "docker",
             "tmux",
             "fish",
-            "telegram-desctop",
-            "mattermost",
+            "telegram-desktop",
+            "mattermost-desktop",
             "make",
             "cmake",
             "fzf",
             "pyenv",
             "tree",
+            "gcc",
         ],
         update=True,
         upgrade=True,
+        _sudo=True,
     )
 
 
+# Map Caps to ESC
+server.shell(
+    commands=[
+        "gsettings set org.gnome.desktop.input-sources xkb-options \"['caps:swapescape']\""
+    ]
+)
+
+# oh-my-fish
 server.shell(
     commands=[
         'sh -c "$(curl -fsSL https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish)"'
@@ -37,14 +47,24 @@ server.shell(
 # fisher
 server.shell(
     commands=[
-        'sh -c "$(curl -fsSL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher)"'
+        'sh -c "$(curl -fsSL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher)"  | grep -e "Already installed" -e "installed"'
     ]
 )
 
+# NVM
 server.shell(commands=['sh -c "$(fisher install jorgebucaran/nvm.fish)"'])
+
 # theme
 server.shell(commands=['sh -c "$(fisher install pure-fish/pure)"'])
+
 # rust
 server.shell(
     commands=['sh -c "$(curl --proto=https --tlsv1.2 -sSf https://sh.rustup.rs | sh)"']
 )
+
+packages()
+
+# TODO:
+# google chrome
+# vscode
+# jetbrains toolbox
